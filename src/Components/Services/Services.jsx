1822e0272fd1bce12013429b2c53de2235c68b25
Alexis-Campomanes <alexiscp.developer@gmail.com>
2022-09-05 12:26:47 -0500

    services-terminado

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -3,6 +3,7 @@ import './Services.scss';
 import HeartEmoji from '../../img/heartemoji.png';
 import Glasses from '../../img/glasses.png';
 import Humble from '../../img/humble.png';
+import Card from '../Card/Card';
 
 const Services = () => {
   return (
@@ -16,14 +17,46 @@ const Services = () => {
         alias optio iusto voluptatem ratione illo nihil, iste eum temporibus id.
         </span>
         <div className="button s-button">Download CV</div>
-        <div className="blur s-blur1" style={{
+        <div className="blur" style={{
           background:'#abf1ff94'
         }}
         ></div>
       </div>
       {/* right side */}
       <div className="cards">
-        Lorem ipsum dolor sit amet cnt iciendis voluptatum.
+        <div className='cards-conteiner' style={{
+          left:'9rem'
+        }}>
+          <Card 
+            emoji = {HeartEmoji}
+            heading = {'Design'}
+            detail = {'Figma, Sketch, Photoshop, Adobe ilustrator, Adobe xd'}
+          />
+        </div>
+        <div className="cards-glasses" style={{
+          top: '20rem',
+          left: '24rem'
+        }}>
+          <Card 
+            emoji = {Glasses}
+            heading = {'Developer'}
+            detail = {'Html, Css, JavaScript, React, NodeJs, Express'}
+          />
+        </div>
+        <div className="cards-humble" style={{
+          top:'28.3rem',
+          left: '-11em'
+        }}>
+          <Card 
+            emoji={Humble}
+            heading = {'UI/UX'}
+            detail = {'Lorem ipsum durmy text are usually use in section where we need some rando text'}
+          />
+        </div>
+        <div className="blur-cards" style={{
+          background: '#edd0ff'
+        }}
+        ></div>
       </div>
     </div>
   )
